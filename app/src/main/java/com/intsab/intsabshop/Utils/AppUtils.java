6f161a6e1f97d0c4401bf7166e0d56d05f821dbd
Intsab <intsab.haider@xische.com>
2021-05-07 05:15:55 +0400

    Utility Methods are added

--- app/src/main/java/com/intsab/intsabshop/Utils/AppUtils.java
+++ app/src/main/java/com/intsab/intsabshop/Utils/AppUtils.java
@@ -1,8 +1,34 @@
 package com.intsab.intsabshop.Utils;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.intsab.intsabshop.R;
+
+import java.net.InetAddress;
+
 /**
  * Created by intsabhaider
  * on 07,May,2021
  */
 class AppUtils {
+
+    public static void loadImage(Context context, String url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .placeholder(R.drawable.ic_category)
+                .into(imageView);
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("https://fakestoreapi.com");
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
